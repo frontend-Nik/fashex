a09d5992afca46132e808e7130870127737fecc6
nikhil gondaliya <nikwebtech@gmail.com>
2023-04-23 12:07:36 +0530

    initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,74 @@
+
+
+// nav js
+
+$(document).ready(function(){
+	$(window).bind('scroll', function() {
+		var navHeight = $('.main-header').height();
+		if ($(window).scrollTop() > navHeight) {
+			$('.nav-bar').addClass('fixed');
+		 }
+		else {
+			$('.nav-bar').removeClass('fixed');
+		 }
+	});
+
+  var path = window.location.href; 
+  // because the 'href' property of the DOM element is the absolute path
+  $('ul a').each(function() {
+    if (this.href === path) {
+      $(this).addClass('active');
+    }
+  });
+});
+  
+
+// Start Banner Hero slider start
+
+"use strict";
+$(document).ready(function () {
+  // Accordion
+  var all_panels = $(".templatemo-accordion > li > ul").hide();
+
+  $(".templatemo-accordion > li > a").click(function () {
+    console.log("Hello world!");
+    var target = $(this).next();
+    if (!target.hasClass("active")) {
+      all_panels.removeClass("active").slideUp();
+      target.addClass("active").slideDown();
+    }
+    return false;
+  });
+  // End accordion
+
+  // Product detail
+  $(".product-links-wap a").click(function () {
+    var this_src = $(this).children("img").attr("src");
+    $("#product-detail").attr("src", this_src);
+    return false;
+  });
+  $("#btn-minus").click(function () {
+    var val = $("#var-value").html();
+    val = val == "1" ? val : val - 1;
+    $("#var-value").html(val);
+    $("#product-quanity").val(val);
+    return false;
+  });
+  $("#btn-plus").click(function () {
+    var val = $("#var-value").html();
+    val++;
+    $("#var-value").html(val);
+    $("#product-quanity").val(val);
+    return false;
+  });
+  $(".btn-size").click(function () {
+    var this_val = $(this).html();
+    $("#product-size").val(this_val);
+    $(".btn-size").removeClass("btn-secondary");
+    $(".btn-size").addClass("btn-success");
+    $(this).removeClass("btn-success");
+    $(this).addClass("btn-secondary");
+    return false;
+  });
+  // End roduct detail
+});
